666680b2d1678e0e95aa97057ba657cba6db86df
1ventorus <113920033+1ventorus@users.noreply.github.com>
2023-12-20 15:28:13 +0100

    Update maj.py

--- maj.py
+++ maj.py
@@ -1,1 +1,31 @@
+#!/usr/bin/env python
 
+import os
+import time
+import urllib.request
+
+def fetch_file(url, filename):
+    urllib.request.urlretrieve(url, filename)
+
+def game_install():
+    fetch_file("https://raw.githubusercontent.com/1ventorus/nuclear_ingenior/main/nuclear_ingenior.py", "nuclear_ingenior.py")
+    print("launcher installé")
+    time.sleep(2)
+
+def requierment_install():
+    fetch_file("https://raw.githubusercontent.com/1ventorus/nuclear_ingenior/main/game_system.py", "game_system.py")
+    fetch_file("https://raw.githubusercontent.com/1ventorus/nuclear_ingenior/main/maj.py", "maj.py")
+    print("requierment installé")
+    time.sleep(2)
+
+if os.path.exists("nuclear_ingenior.py"):
+    os.remove("nuclear_ingenior.py")
+
+if os.path.exists("game_system.py"):
+    os.remove("game_system.py")
+
+if os.path.exists("maj.py"):
+    os.remove("maj.py")
+
+game_install()
+requierment_install()
